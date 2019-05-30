e546c2c93135fff91836bfd55428b5846a4a4885
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-05-30 16:51:17 +0300

    Update Router.php

--- src/Router.php
+++ src/Router.php
@@ -1,334 +1,183 @@
 <?php
 
-/**
- * The PSR RESTful Router.
- *
- * @package dionchaika/router
- * @version 1.0.0
- * @license MIT
- * @author Dion Chaika <dionchaika@gmail.com>
- */
-
-namespace Dionchaika\Router;
-
-use Closure;
-use InvalidArgumentException;
-use Psr\Http\Message\UriInterface;
-use Dionchaika\Container\Container;
-use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+namespace Lazy\Router;
 
 class Router
 {
     /**
-     * The allowed request methods.
+     * The allowed methods.
      */
-    const METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'];
+    const ALLOWED_METHODS = [
+
+        'GET',
+        'HEAD',
+        'POST',
+        'PUT',
+        'PATCH',
+        'DELETE',
+        'OPTIONS'
+
+    ];
 
     /**
-     * The route collection.
-     *
-     * @var \Dionchaika\Router\RouteCollection
-     */
-    protected $routes;
-
-    /**
-     * The container
-     * instance used by the router.
-     *
-     * @var \Psr\Container\ContainerInterface
-     */
-    protected $container;
-
-    /**
-     * The array
-     * of router middleware.
+     * The array of routes.
      *
      * @var mixed[]
      */
-    protected $middleware = [];
+    protected $routes = [];
 
     /**
-     * The array
-     * of route group attributes.
+     * The array of all of the routes.
      *
      * @var mixed[]
      */
-    protected $routeGroup = [];
+    protected $allRoutes = [];
 
     /**
-     * The request base path.
+     * The array of named routes.
      *
-     * @var string
+     * @var mixed[]
      */
-    protected $requestBasePath = '';
+    protected $namedRoutes = [];
 
     /**
-     * @param \Dionchaika\Router\RouteCollection|null $routes
-     * @param \Psr\Container\ContainerInterface|null  $container
-     */
-    public function __construct(
-        ?RouteCollection $routes       = null,
-        ?ContainerInterface $container = null
-    ) {
-        $this->routes = $routes ?? new RouteCollection;
-        $this->container = $container ?? new Container;
-    }
-
-    /**
-     * Get the container
-     * instance used by the router.
+     * The route group stack.
      *
-     * @return \Psr\Container\ContainerInterface
+     * @var mixed[]
      */
-    public function getContainer(): ContainerInterface
+    protected $routeGroupStack = [];
+
+    /**
+     * Get the array of all of the routes.
+     *
+     * @return \Lazy\Router\Route[]
+     */
+    public function getRoutes(): array
     {
-        return $this->container;
+        return array_values($this->allRoutes);
     }
 
     /**
-     * Set the container
-     * instance used by the router.
+     * Register a GET method route.
      *
-     * @param \Psr\Container\ContainerInterface $container
-     * @return self
-     */
-    public function setContainer(ContainerInterface $container): self
-    {
-        $this->container = $container;
-        return $this;
-    }
-
-    /**
-     * Get the request base path.
+     * @param  string  $pattern
+     * @param  mixed  $handler
      *
-     * @return string
-     */
-    public function getRequestBasePath(): string
-    {
-        return $this->requestBasePath;
-    }
-
-    /**
-     * Set the request base path.
-     *
-     * @param string $path
-     * @return self
-     */
-    public function setRequestBasePath(string $path): self
-    {
-        $this->requestBasePath = '/'.trim($path, '/');
-        return $this;
-    }
-
-    /**
-     * Add a new router middleware.
-     *
-     * @param \Psr\Http\Server\MiddlewareInterface|\Closure|string $middleware
-     * @return self
-     */
-    public function use($middleware): self
-    {
-        $this->middleware[] = $middleware;
-        return $this;
-    }
-
-    /**
-     * Add a new GET method route.
-     *
-     * @param string                                                   $pattern
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
+     * @return \Lazy\Router\Route
      */
     public function get(string $pattern, $handler): Route
     {
-        return $this->addRoute('GET|HEAD', $pattern, $handler);
+        return $this->route('GET|HEAD', $pattern, $handler);
     }
 
     /**
-     * Add a new HEAD method route.
+     * Register a HEAD method route.
      *
-     * @param string                                                   $pattern
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
+     * @param  string  $pattern
+     * @param  mixed  $handler
+     *
+     * @return \Lazy\Router\Route
      */
     public function head(string $pattern, $handler): Route
     {
-        return $this->addRoute('HEAD', $pattern, $handler);
+        return $this->route('HEAD', $pattern, $handler);
     }
 
     /**
-     * Add a new POST method route.
+     * Register a POST method route.
      *
-     * @param string                                                   $pattern
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
+     * @param  string  $pattern
+     * @param  mixed  $handler
+     *
+     * @return \Lazy\Router\Route
      */
     public function post(string $pattern, $handler): Route
     {
-        return $this->addRoute('POST', $pattern, $handler);
+        return $this->route('POST', $pattern, $handler);
     }
 
     /**
-     * Add a new PUT method route.
+     * Register a PUT method route.
      *
-     * @param string                                                   $pattern
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
+     * @param  string  $pattern
+     * @param  mixed  $handler
+     *
+     * @return \Lazy\Router\Route
      */
     public function put(string $pattern, $handler): Route
     {
-        return $this->addRoute('PUT', $pattern, $handler);
+        return $this->route('PUT', $pattern, $handler);
     }
 
     /**
-     * Add a new PATCH method route.
+     * Register a PATCH method route.
      *
-     * @param string                                                   $pattern
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
+     * @param  string  $pattern
+     * @param  mixed  $handler
+     *
+     * @return \Lazy\Router\Route
      */
     public function patch(string $pattern, $handler): Route
     {
-        return $this->addRoute('PATCH', $pattern, $handler);
+        return $this->route('PATCH', $pattern, $handler);
     }
 
     /**
-     * Add a new DELETE method route.
+     * Register a DELETE method route.
      *
-     * @param string                                                   $pattern
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
+     * @param  string  $pattern
+     * @param  mixed  $handler
+     *
+     * @return \Lazy\Router\Route
      */
     public function delete(string $pattern, $handler): Route
     {
-        return $this->addRoute('DELETE', $pattern, $handler);
+        return $this->route('DELETE', $pattern, $handler);
     }
 
     /**
-     * Add a new OPTIONS method route.
+     * Register an OPTIONS method route.
      *
-     * @param string                                                   $pattern
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
+     * @param  string  $pattern
+     * @param  mixed  $handler
+     *
+     * @return \Lazy\Router\Route
      */
     public function options(string $pattern, $handler): Route
     {
-        return $this->addRoute('OPTIONS', $pattern, $handler);
+        return $this->route('OPTIONS', $pattern, $handler);
     }
 
     /**
-     * Add a new any method route.
+     * Register an any method route.
      *
-     * @param string                                                   $pattern
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
+     * @param  string  $pattern
+     * @param  mixed  $handler
+     *
+     * @return \Lazy\Router\Route
      */
     public function any(string $pattern, $handler): Route
     {
-        return $this->addRoute(self::METHODS, $pattern, $handler);
+        return $this->route(self::ALLOWED_METHODS, $pattern, $handler);
     }
 
     /**
-     * Add a new fallback route.
+     * Register a route.
      *
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
+     * @param  mixed  $methods
+     * @param  string  $pattern
+     * @param  mixed  $handler
+     *
+     * @return \Lazy\Router\Route
      */
-    public function fallback($handler): Route
+    public function route($methods, string $pattern, $handler): Route
     {
-        return $this->any('.*', $handler);
-    }
+        $route = new Route($methods, $pattern, $handler);
 
-    /**
-     * Add a new route.
-     *
-     * @param string|string[]                                          $methods
-     * @param string                                                   $pattern
-     * @param \Psr\Http\Server\RequestHandlerInterface|\Closure|string $handler
-     * @return \Dionchaika\Router\Route
-     */
-    public function addRoute($methods, string $pattern, $handler): Route
-    {
-        return $this->routes->add(new Route($methods, $pattern, $handler));
-    }
-
-    /**
-     * Add a new route group.
-     *
-     * @param mixed[]  $attributes
-     * @param \Closure $callback
-     * @return void
-     */
-    public function group(array $attributes = [], Closure $callback): void
-    {
-        $this->routeGroup[] = $attributes;
-        $callback($this);
-        array_pop($this->routeGroup);
-    }
-
-    /**
-     * Match a request and return a response.
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function matchRequest(ServerRequestInterface $request): ResponseInterface
-    {
-        if ('' !== $this->requestBasePath) {
-            $request = $request->withUri(
-                $request->getUri()->withPath(
-                    str_replace($this->requestBasePath, '', $request->getUri()->getPath())
-                )
-            );
+        foreach ($route->getMethods() as $method) {
+            $this->routes[$method][$pattern] = $route;
         }
 
-        foreach ($this->routes->all() as $route) {
-            if ($route->isMatchesRequest($request)) {
-                $request = $request->withAttribute('route', $route);
-
-                foreach ($route->getParameters()->all() as $parameter) {
-                    $request = $request->withAttribute(
-                        $parameter->getName(),
-                        $parameter->getValue()
-                    );
-                }
-
-                $handler = new RequestHandler(function ($request) use ($route) {
-                    return $route
-                        ->getHandler()
-                        ->setContainer($this->container)
-                        ->handle($request);
-                }, $this->middleware);
-
-                return $handler
-                    ->setContainer($this->container)
-                    ->handle($request);
-            }
-        }
-
-        throw new NotFoundException;
-    }
-
-    /**
-     * Get the URI for the route.
-     *
-     * @param string  $name
-     * @param mixed[] $parameters
-     * @return \Psr\Http\Message\UriInterface
-     * @throws \InvalidArgumentException
-     */
-    public function getUriFor(string $name, array $parameters = []): UriInterface
-    {
-        $this->routes->updateNamedRoutes();
-
-        if ($this->routes->has($name)) {
-            return $this->routes->get($name)->getUri($parameters);
-        }
-
-        throw new InvalidArgumentException(
-            'Route does not exists: '.$name.'!'
-        );
+        return $this->allRoutes[implode('|', $route->getMethods()).' '.$pattern] = $route;
     }
 }
